2a71c6ccbc670389e94490105b399cbf391d7484
Jamie Canning <canningjamie@gmail.com>
2022-12-30 21:01:14 +0000

    Add initial MVC files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JamiesRecipes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
