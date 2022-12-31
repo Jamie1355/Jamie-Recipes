dd6daba0f5f2b0c3ed208608c8bcd6779e2a5e1b
Jamie Canning <canningjamie@gmail.com>
2022-12-31 15:21:46 +0000

    Correct spelling of Macaroni

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -25,7 +25,7 @@ public static class SeedData
                     Title = "Mac and Cheese",
                     Description = "A dirty Mac & Cheese - the perfect alternative to a greasy takeaway!",
                     Serves = 2,
-                    Ingredients = "6 handfuls Macoroni Pasta, Some Broccoli, A knob of butter, A heaped tablespoon of flour, Lots of Milk, Lots of Cheese",
+                    Ingredients = "6 handfuls Macaroni Pasta, Some Broccoli, A knob of butter, A heaped tablespoon of flour, Lots of Milk, Lots of Cheese",
                     Method = "1. Boil pasta and broccoli in a pan. 2. Melt butter in second pan. 3. Add flour to melted butter and mix. 4. Slowly add milk to butter and flour mixture until smooth-ish, then add cheese. 5. Drain pasta/broccoli and add to baking tray. 6. Mix sauce into baking tray with pasta. 7. Put in oven until looking a tad crispy. 8. Voila!",
                     Dietary = "V"
                 }
