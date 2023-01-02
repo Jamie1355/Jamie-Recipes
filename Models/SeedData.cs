dd5b1695ef6c07a97aa289a9c2e93a25ad1908fc
Jamie Canning <canningjamie@gmail.com>
2023-01-02 18:19:53 +0000

    Add \n to SeedData

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -25,8 +25,8 @@ public static class SeedData
                     Title = "Mac and Cheese",
                     Description = "A dirty Mac & Cheese - the perfect alternative to a greasy takeaway!",
                     Serves = 2,
-                    Ingredients = "6 handfuls Macaroni Pasta, Some Broccoli, A knob of butter, A heaped tablespoon of flour, Lots of Milk, Lots of Cheese",
-                    Method = "1. Boil pasta and broccoli in a pan. 2. Melt butter in second pan. 3. Add flour to melted butter and mix. 4. Slowly add milk to butter and flour mixture until smooth-ish, then add cheese. 5. Drain pasta/broccoli and add to baking tray. 6. Mix sauce into baking tray with pasta. 7. Put in oven until looking a tad crispy. 8. Voila!",
+                    Ingredients = "6 handfuls Macaroni Pasta\nSome Broccoli\nA knob of butter\nA heaped tablespoon of flour\nLots of Milk\nLots of Cheese",
+                    Method = "1. Boil pasta and broccoli in a pan.\n2. Melt butter in second pan.\n3. Add flour to melted butter and mix.\n4. Slowly add milk to butter and flour mixture until smooth-ish, then add cheese.\n5. Drain pasta/broccoli and add to baking tray.\n6. Mix sauce into baking tray with pasta.\n7. Put in oven until looking a tad crispy.\n8. Voila!",
                     Dietary = "V"
                 }
             );
