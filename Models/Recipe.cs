6c283ab7d1b3b68b6b229dcc65fc327185194028
Jamie Canning <canningjamie@gmail.com>
2023-01-02 19:55:43 +0000

    Add some validation to recipe fields

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -4,16 +4,28 @@ namespace JamiesRecipes.Models;
 
 public class Recipe{
     public int Id { get; set; }
+
+    [Required]
     public string? Title { get; set; }
+
+    [Required]
+    [StringLength(500)]
     public string? Description { get; set; }
+
+    [Required]
+    [Range(1, 20)]
     public int Serves { get; set; }
 
+    [Required]
     [DataType(DataType.MultilineText)]
     [UIHint("DisplayRecipe")]
     public string? Ingredients { get; set; }
 
+    [Required]
     [DataType(DataType.MultilineText)]
     [UIHint("DisplayRecipe")]
     public string? Method { get; set; }
+
+    [Required]
     public string? Dietary { get; set; }
 }
