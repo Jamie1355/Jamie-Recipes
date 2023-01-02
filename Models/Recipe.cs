589be94588b7e80b5d8f55603b74943e74223fac
Jamie Canning <canningjamie@gmail.com>
2023-01-02 18:10:38 +0000

    Display ingredients and method as multiline

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -7,7 +7,13 @@ public class Recipe{
     public string? Title { get; set; }
     public string? Description { get; set; }
     public int Serves { get; set; }
+
+    [DataType(DataType.MultilineText)]
+    [UIHint("DisplayRecipe")]
     public string? Ingredients { get; set; }
+
+    [DataType(DataType.MultilineText)]
+    [UIHint("DisplayRecipe")]
     public string? Method { get; set; }
     public string? Dietary { get; set; }
 }
